b29afb04d3b75189850b67611a4d831db7ca5907
Sergeevna <l.shliak@docsinbox.ru>
2022-10-05 18:45:52 +0300

    Add Service

---
+++ src/test/java/ru/netology/cashbackhacker/CashBackHackerServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.cashbackhacker;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerServiceTest {
+    CashBackHackerService service = new CashBackHackerService();
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 2200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
